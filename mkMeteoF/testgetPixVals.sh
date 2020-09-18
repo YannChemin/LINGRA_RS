cacf1ae80eec7cf77658203308f47ad2850a83f2
Yann Chemin <ychemin@gmail.com>
2020-09-18 12:25:10 +0200

    debugged some more

--- mkMeteoF/testgetPixVals.sh
+++ mkMeteoF/testgetPixVals.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 from libera5 import *
-result=getPixelVals('/Users/dnd/Documents/ERA5/ERA5_EU_2020.nc','ssrd',-10.0,60.0)
+result=getPixelVals('/Users/dnd/Documents/ERA5/ERA5_EU_2020.nc','ssrd',0.0,47.0)
 print(result)
 
